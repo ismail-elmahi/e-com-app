e74c27a709adc464df34d6d33023440d6874ecbc
ismail el mahi <ismail.elmahi1@gmail.com>
2019-11-03 14:00:01 +0100

    update01 price

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -17,7 +17,7 @@ const CartItem = ({item , value}) => {
                 {title}
             </div>
             <div className="col-10 mx-auto col-lg-2">
-                <span className="d-lg-none">price:</span>
+                <span className="d-lg-none">price:$</span>
                 {price}
             </div>
             <div className="col-10 col-lg-2 mx-auto my-2 my-lg-0">
